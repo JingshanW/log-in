3e7b6f76c31a3451dcd084324b58fbe689eed480
jingshanW <jw3435@tc.columbia.edu>
2017-04-03 20:25:37 -0400

    finish

--- app/src/main/java/com/example/android/login/MainActivity.java
+++ app/src/main/java/com/example/android/login/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.android.login;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity
+{
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener authListener;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authListener = new FirebaseAuth.AuthStateListener()
+        {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
+            {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null){
+                    Intent i=new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(i);
+                }
+            }
+
+        };
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        auth.removeAuthStateListener(authListener);
+    }
+
+    public void signOut(View view) {
+        auth.signOut();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    public void addSomething(View view)
+    {
+        FirebaseUser user = auth.getCurrentUser();
+        DatabaseReference userRef = database.getReference(user.getUid());
+        userRef.push().setValue(Math.random());
     }
 }
+
